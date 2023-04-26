3a4664ee3c9a87ccfa513f015e6831a93245c90f
ivcor <iv.cordasic@gmail.com>
2023-04-26 18:34:25 -0400

    Done in1To10

--- src/Logic_1/in1To10.java
+++ src/Logic_1/in1To10.java
@@ -3,7 +3,11 @@ package Logic_1;
 public class in1To10 {
 
     public boolean in1To10(int n, boolean outsideMode) {
-        
+        if (outsideMode){
+            return n <= 1 || n >= 10;
+        }else{
+            return n >= 1 && n <= 10;
+        }
     }
 
 
